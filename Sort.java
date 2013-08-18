44ff8b07088eac401db7c6ba2e1896815588d1df
kharepratyush <khare.pratyush@outlook.com>
2013-08-18 23:11:57 +0530

    Sorting

--- Sort.java
+++ Sort.java
@@ -18,4 +18,48 @@ class Sort
 			}
 		}
 	}
+	void Merge(int a[],int low,int mid,int high)
+	{
+		int b[]=new int[high];
+		int h=low;
+		int i=low;
+		int j=mid+1;
+		while((h<=mid)&&(j<=high))
+		{
+			if(a[h]<a[j])
+			{
+				b[i]=a[h];
+				h++;
+			}
+			else
+			{
+				b[i]=a[j];
+				j++;
+			}
+			i++;
+		}
+		if(h>mid)
+		{
+			for(int k=j;k<=high;k++)
+				b[i++]=a[k];
+		}
+		else
+		{
+			for(int k=h;k<=mid;k++)
+				b[i++]=a[k];
+		}
+		for(int k=low;k<=high;k++)
+			a[i]=b[i];
+	}
+
+	void MergeSort(int a[],int low,int high)
+	{
+		if(low<high)
+		{
+			int mid=(low+high)/2;
+			MergeSort(a,low,mid);
+			MergeSort(a,mid,high);
+			Merge(a,low,mid,high);
+		}
+	}
 }
