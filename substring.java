82d88c28832836ee904d97cd2cc68e867ea39ea0
kharepratyush <khare.pratyush@outlook.com>
2014-08-26 18:16:20 +0530

    Substrings seperated by space

---
+++ substring.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.lang.*;
+class substring
+{
+	public static void main(String args[])throws IOException
+	{
+		InputStreamReader isr=new InputStreamReader(System.in);
+		BufferedReader d1=new BufferedReader(isr);
+		String str;
+		System.out.println("Enter the string");
+		str=d1.readLine();
+		int i=0;
+		int j=0;
+		while(i<str.length())
+		{
+			if(str.charAt(i)==' ')
+			{
+				System.out.println(str.substring(j,i)+"\nLength is "+str.substring(j,i).length());
+				j=i+1;
+			}
+			i++;
+			if(i==str.length())
+				System.out.println(str.substring(j,i)+"\nLength is "+str.substring(j,i).length());
+
+		}
+	}
+}
