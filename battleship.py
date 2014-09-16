4a08e19c3c0da646c53265e12ded38ba388195d0
kharepratyush <khare.pratyush@outlook.com>
2014-09-16 15:41:06 +0530

    Basic Minesweeper Style Game

--- battleship.py
+++ battleship.py
@@ -18,28 +18,39 @@ def random_row(board):
 def random_col(board):
     return randint(0, len(board[0]) - 1)
 
+
+
 ship_row = random_row(board)
 ship_col = random_col(board)
-#print ship_row
-#print ship_col
+print ship_row
+print ship_col
+
+i=ship_row-1
+
+#print_board(board_hidden)
+
 for i in range(4):
-# Everything from here on should go in your for loop!
-# Be sure to indent four spaces!
-    guess_row = int(raw_input("Guess Row:"))
-    guess_col = int(raw_input("Guess Col:"))
+
+    guess_row = int(raw_input("Guess Row:"))-1
+    guess_col = int(raw_input("Guess Col:"))-1
 
     if guess_row == ship_row and guess_col == ship_col:
         print "Congratulations! You sunk my battleship!"
+        board[guess_row][guess_col] = "W"
+        print_board(board)
         break;
     else:
         if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
             print "Oops, that's not even in the ocean."
+            print_board(board)
         elif(board[guess_row][guess_col] == "X"):
             print "You guessed that one already."
+            print_board(board)
         else:
             print "You missed my battleship!"
             board[guess_row][guess_col] = "X"
+            print_board(board)
     print i+1
     if i>3:
         print "Game Over"
-print_board(board)
+#print_board(board)
